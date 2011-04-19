305061bb78881bb6f8b0d49071c92a3748b66406
rmdrimmie <rob.drimmie@gmail.com>
2011-04-18 22:35:05 -0400

    Trying to write a test for accessing the server but... I think I don't have it right for asynchronous behaviour.

--- test/use-node-xmpp.js
+++ test/use-node-xmpp.js
@@ -1,4 +1,4 @@
-exports.testNodeXmpp = function(test){
+exports.testNodeXmppExists = function(test){
   var success = false;
 
   test.expect(1);
@@ -11,5 +11,36 @@ exports.testNodeXmpp = function(test){
   }
 
   test.ok( success, "Node-XMPP exists and is included." );
+  test.done();
+};
+
+exports.testOnline = function(test) {
+  var success = false,
+      jid = "robtestone@gmail.com",
+      pwd = "robtest1";
+
+  test.expect(1);
+
+  var cl = new xmpp.Client({ 
+    jid: jid,
+    password: pwd }
+  );
+
+  cl.on(
+    'online',
+    function() {
+      console.log( "HELLO" );
+      success = true;
+      test.ok( success, "Authorized and online." );
+    }
+  );
+
+  cl.on(
+    'error',
+    function(e) {
+      console.log(e);
+    }
+  );
+
   test.done();
 };
