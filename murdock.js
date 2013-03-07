607e775bfe81e91b04638b4a2fda6b060bd985cd
Rob Drimmie <rob@drimmie.net>
2013-03-06 22:56:44 -0500

    Loading murdules.json and files, not yet routing into them. Next step is to create a delayed echo with a setTimeout and send the message to both to make sure this part is working okay.

--- murdock.js
+++ murdock.js
@@ -1,7 +1,9 @@
 var sys = require( 'sys' );
 var xmpp = require( 'simple-xmpp' );
 var util = require( 'util' );
-var murdule = require( './murdules/echo');
+var fs = require( 'fs' );
+
+var allMurdules = Array();
 
 var argv = process.argv;
 
@@ -16,6 +18,36 @@ xmpp.connect({
   host: 'talk.google.com',
   port: 5222
 });
+
+// open the murdules.json file, get the array of murdules, load them all
+// @todo genercise these file functions
+var murdulesFile = './murdules.json';
+var murdulesPath = './murdules/';
+var murdulesFiles = [];
+fs.exists( murdulesFile, function murdulesFileExists( exists ) {
+  if( exists ) {
+    fs.readFile( murdulesFile, function loadMurdulesFiles( err, data ) {
+      murdulesFiles = JSON.parse( data );
+
+      var thisMF;
+      for( mfIdx = 0; mfIdx < murdulesFiles.length; mfIdx++ ) {
+        thisMF = murdulesPath + murdulesFiles[mfIdx];
+
+        fs.exists( thisMF, function openMurduleFile( mfExists ) {
+          if( mfExists ) {
+            allMurdules.push( require(thisMF) );
+          } else {
+            console.error( 'thisMF does not exist: ' + thisMF );
+          }
+        });
+      }
+    });
+  } else {
+    console.error( 'murdules.json does not exist' );
+  }
+});
+
+
 
 xmpp.on('online', function() {
   console.log( "\nechoing\n" );
